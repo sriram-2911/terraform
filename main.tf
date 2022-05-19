b17dea125ddd0e3611d6e1cc5adfde99282eeba4
sriram kadir <85162129+sriram-2911@users.noreply.github.com>
2022-05-19 19:11:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 resource "aws_ecr_repository" "repository" {
   name                 = var.name
   image_tag_mutability = "IMMUTABLE"
